44a7ece7d6ee8029def4b3005f71f27db752c866
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-15 16:40:52 +0600

    [#2] frature: api endpoints added

---
+++ src/constant.py
@@ -0,0 +1,11 @@
+class Constant(object):
+    """Class for organizing constants."""
+
+    # website names
+    CODEFORCES = 'codeforces'
+
+    # API endpoints
+    USER_INFO = 'https://codeforces.com/api/user.info?handles={0}'
+    USER_STATUS = 'https://codeforces.com/api/user.status?handle={0}'
+    USER_RATING = 'https://codeforces.com/api/user.rating?handle={0}'
+
