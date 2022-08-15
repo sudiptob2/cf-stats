2df88dcdecd199903de6dce7f9fa859f6310217b
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-15 16:40:21 +0600

    [#2] frature: codeforces response parser updated

---
+++ src/utils/cf_response_parser.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+from src.models.user import User
+from src.utils.cf_request_handler import CFRequestHandler
+
+
+class CFResponseParser:
+    """Parse responses of codeforces API."""
+
+    @staticmethod
+    def parse():
+        """Parse and make user object."""
+        CFRequestHandler.make_request()
+        CFResponseParser.__parse_user_info(CFRequestHandler.user_info)
+        CFResponseParser.__parse_user_submission(CFRequestHandler.user_submission)
+        CFResponseParser.__parse_rating_changes(CFRequestHandler.rating_changes)
+
+    @classmethod
+    def __parse_user_info(cls, user_info):
+        """Parse and sets user's basic profile."""
+        user = User()
+        user.name = user_info.get('firstName', '') + ' ' + user_info.get('lastName', '')
+        user.organization = user_info.get('organization', '')
+        user.rating = user_info.get('rating', 0)
+        user.rank = user_info.get('rank', 'newbie')
+        user.max_rating = user_info.get('rating', 0)
+        user.max_rank = user_info.get('maxRank', 'newbie')
+        user.contributions = user_info.get('maxRating', 0)
+
+    @classmethod
+    def __parse_user_submission(cls, user_submission):
+        """Parse and save submission related details."""
+        user = User()
+        user.submissions = len(user_submission)
+        freq = defaultdict(lambda: 0)
+        for sb in user_submission:
+            freq[sb['verdict']] += 1
+
+        user.accepted = freq['OK']
+        user.wrong_ans = freq['TIME_LIMIT_EXCEEDED']
+        user.tle = freq['TIME_LIMIT_EXCEEDED']
+
+    @classmethod
+    def __parse_rating_changes(cls, rating_changes):
+        """Sets total number of contests participated by the user."""
+        user = User()
+        user.contests = len(rating_changes)
+
+
+if __name__ == '__main__':
+    CFResponseParser.parse()
+    test_user = User()
+    print(test_user)
