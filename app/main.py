12a112701c2fe5107302c641ebb85969305c5604
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-16 18:13:21 +0600

    [#3] feature: rating and max rating generation example added in the main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
+from app.constant import Constant
 from app.services.cf_response_parser import CFResponseParser
 from app.services.svg_factory import SVGFactory
+from app.utils.file_utils import Config
 
 
 def main() -> None:
     """This method is used for testing purpose."""
     print('codeforces stat is coming...')
     CFResponseParser.parse()
-    generator = SVGFactory.get_svg_generator('rating_badge')
-    generator.generate()
+    generator = SVGFactory.get_svg_generator(Constant.BADGE)
+
+    config = Config(Constant.RATING)
+    generator.generate(config)
+
+    config = Config(Constant.MAX_RATING)
+    generator.generate(config)
+    print('Images are generated in the output folder.')
 
 
 if __name__ == '__main__':
