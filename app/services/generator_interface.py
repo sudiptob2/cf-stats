edc1ef684e6512bdadaee2e3c8227be4f62717e9
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-16 18:07:57 +0600

    [#3] feature: generator interface updated, a config is passed in the generate class to hold the configuration of the svg.

--- app/services/generator_interface.py
+++ app/services/generator_interface.py
@@ -1,10 +1,12 @@
 from abc import ABCMeta, abstractmethod
 
+from app.utils.file_utils import Config
+
 
 class IGenerator(metaclass=ABCMeta):
     """Interface for creating svg handler class."""
 
     @classmethod
     @abstractmethod
-    def generate(cls):
+    def generate(cls, config: Config):
         """An abstract interface method"""
