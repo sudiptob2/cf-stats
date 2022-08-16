15c45a1025495d9c39f27e1666d33f07c2a79c70
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-16 18:09:06 +0600

    [#3] refactor: badge generator accepts configuration and generates badges according to the given config

--- app/services/rating_badge_generator.py
+++ app/services/rating_badge_generator.py
@@ -3,13 +3,13 @@ import pybadges
 from app.constant import Constant
 from app.models.user import User
 from app.services.generator_interface import IGenerator
-from app.utils.file_utils import FileHelper, FileContext
+from app.utils.file_utils import FileHelper, FileContext, Config
 
 
-class RatingBadgeGenerator(IGenerator, FileHelper):
+class BadgeGenerator(IGenerator, FileHelper):
 
     @classmethod
-    def generate(cls):
+    def generate(cls, config: Config):
         """Generate svg badge for rating."""
         user = User()
         badge = pybadges.badge(
@@ -18,6 +18,6 @@ class RatingBadgeGenerator(IGenerator, FileHelper):
             right_text=str(user.rating),
             right_color=str(user.rating_color)
         )
-        file_context = FileContext(badge, f"{Constant.OUTPUT_FOLDER}/{Constant.RATING_BADGE}.svg")
+        file_context = FileContext(badge, f"{Constant.OUTPUT_FOLDER}/{config.badge_type}.svg")
         cls.save_svg(file_context)
         return badge
