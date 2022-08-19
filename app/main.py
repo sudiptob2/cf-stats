638944c58243d297a5ced7756f4fff63a7b9b3be
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-19 23:02:28 +0600

    [#18] feature: added driver code for generating stat card

--- app/main.py
+++ app/main.py
@@ -6,16 +6,25 @@ from app.utils.file_utils import Config
 
 def main() -> None:
     """This method is used for testing purpose."""
-    print('codeforces stat is coming...')
+    print('generating stats...')
     CFResponseParser.parse()
+
     generator = SVGFactory.get_svg_generator(Constant.BADGE)
 
+    print('generating badges...')
     config = Config(Constant.RATING)
     generator.generate(config)
 
     config = Config(Constant.MAX_RATING)
     generator.generate(config)
-    print('Images are generated in the output folder.')
+    print('badge generation complete.')
+
+    print('generating stat card...')
+    generator = SVGFactory.get_svg_generator(Constant.CARD)
+    config = Config(Constant.LIGHT_CARD)
+    generator.generate(config)
+    print('card generation complete.')
+    print('output images are saved in the output folder.')
 
 
 if __name__ == '__main__':
