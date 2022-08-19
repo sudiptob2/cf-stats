edc5eb618d254e7caf499a3fada8788997223254
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-19 21:29:47 +0600

    [#13] feature: ignoring preposing while generating acronym defined in env

--- config/config.py
+++ config/config.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from pydantic import BaseSettings
@@ -6,12 +5,13 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     """Settings class for defining all variables."""
-    cf_handle: str = 'sudipto.me'
+    cf_handle: str = 'DmitriyH'
     base_dir: str = str(Path(__file__).parent.parent)
+    acronym_ignore: list = ['and']
 
     class Config:
         """Config class."""
-        env_file = os.path.expanduser('config/.env')
+        env_file = str(Path(__file__).parent.parent) + '/config/.env'
 
 
 settings = Settings()
