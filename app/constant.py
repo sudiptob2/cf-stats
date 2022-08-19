c30c2d9a6f4e83e36ed5697ccfc41696e860eac3
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-20 02:25:22 +0600

    [#5] cflogo url updated

--- app/constant.py
+++ app/constant.py
@@ -6,7 +6,7 @@ class Constant:
 
     # website names
     CODEFORCES = 'codeforces'
-    LOGO = "https://raw.githubusercontent.com/sudiptob2/cf-stats/master/assets/cflogo.svg"
+    LOGO = 'https://raw.githubusercontent.com/sudiptob2/cf-stats/feature/5-github-actions/assets/cflogo.svg'
 
     # API endpoints
     USER_INFO = 'https://codeforces.com/api/user.info?handles={0}'
